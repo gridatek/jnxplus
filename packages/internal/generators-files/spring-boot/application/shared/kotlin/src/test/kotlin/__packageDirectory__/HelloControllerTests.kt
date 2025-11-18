5fea634867256a66a182db0df0c3aea43a04aa90
kgridou <32600911+kgridou@users.noreply.github.com>
2025-11-18 17:31:39 +0100

    build: wip

--- packages/internal/generators-files/spring-boot/application/shared/kotlin/src/test/kotlin/__packageDirectory__/HelloControllerTests.kt
+++ packages/internal/generators-files/spring-boot/application/shared/kotlin/src/test/kotlin/__packageDirectory__/HelloControllerTests.kt
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.resttestclient.TestRestTemplate
+import org.springframework.boot.resttestclient.getForEntity
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
